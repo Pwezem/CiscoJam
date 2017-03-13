81f5849dce9520049b9f80a969d5bb958e1e4433
Tom <pwezem@gmail.com>
2017-03-13 22:16:52 +0000

    Changed bot core to support newly named trivia handler

--- 2017/challenge3/sparkBot/botCore.py
+++ 2017/challenge3/sparkBot/botCore.py
@@ -3,7 +3,7 @@ import importlib
 from spark2d2.bot import Bot
 
 handler_modules = ['repeater.Repeater', 'futurama.Futurama', 'eightball.Eightball', 'shutdown.ShutDown',
-                   'vote.Vote', 'meaningoflife.MeaningOfLife', 'numbers.Numbers', 'jeopradytrivia.JeopradyTrivia']
+                   'vote.Vote', 'meaningoflife.MeaningOfLife', 'numbers.Numbers', 'trivia.Trivia']
 
 
 class botCore(Bot):
