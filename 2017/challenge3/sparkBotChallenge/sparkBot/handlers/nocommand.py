21c848c22aff068c5a39dc2efab66a0ba9967213
Killian Moore <kilmoore@cisco.com>
2017-03-30 11:12:38 +0100

    nocommand decide to no command, and botcore  decided to core....weird right?

--- 2017/challenge3/sparkBotChallenge/sparkBot/handlers/nocommand.py
+++ 2017/challenge3/sparkBotChallenge/sparkBot/handlers/nocommand.py
@@ -5,13 +5,13 @@ import random
 class NoCommand(MessageHandler):
     def __init__(self):
         self.responses = ["**WHAT** %s?", "Huh, did you say something %s?",
-                          "Look, I know you want me but please stop with the silence %s!",
+                          "Look, please stop with the silence %s!",
                           "Love when people call out my name and then say nothing.",
                           "%s"]
 
     def handle_message(self, raw_msg, user_email, username):
         if "No Command" in raw_msg:
-            return random.choice(self.responses) % username
+            return random.choice(self.responses) % str(username)
         return False
 
     def help(self):
